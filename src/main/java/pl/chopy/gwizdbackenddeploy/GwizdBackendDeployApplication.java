a348ead47a80389835efba9b2e58b41cb8a327f2
marcinbator <marcinbator.ofc@gmail.com>
2023-09-30 12:20:50 +0200

    initial commit

---
+++ src/main/java/pl/chopy/gwizdbackenddeploy/GwizdBackendDeployApplication.java
@@ -0,0 +1,13 @@
+package pl.chopy.gwizdbackenddeploy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GwizdBackendDeployApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GwizdBackendDeployApplication.class, args);
+	}
+
+}
